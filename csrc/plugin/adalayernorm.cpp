8dfc6d54d3fbba3f0809c73697328a8d88b7cbc8
chensiyu47 <chensiyu49@huawei.com>
2026-02-04 20:12:33 +0800

    [feature]新增adaLayernormv2的plugin
    Co-authored-by: chensiyu47<chensiyu49@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !172 merge dev into dev
    
    [feature]新增adaLayernormv2的plugin
    
    Created-by: chensiyu47
    Commit-by: chensiyu47
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20251224
    -->
    # Purpose
    新增adaLayernormv2的plugin
    
    # Test Plan
    单算子测试
    
    # Test Report
    ![image.png](https://raw.gitcode.com/user-images/assets/8476587/d1f7d2cb-b67e-46fe-80e2-b48f2e1dade2/image.png 'image.png')
    
    
    See merge request: Ascend/MindIE-SD!172

--- csrc/plugin/adalayernorm.cpp
+++ csrc/plugin/adalayernorm.cpp
@@ -21,6 +21,9 @@
 using namespace at;
 
 constexpr std::string_view ADALAYER_NORM_OP_NAME = "aclnnAdaLayerNorm";
+constexpr std::string_view ADALAYER_NORM_V2_OP_NAME = "aclnnAdaLayerNormV2";
+constexpr int EXPECTED_TENSOR_DIMENSION = 3;
+
 at::Tensor adaln_mindie_sd_impl_npu(
     const at::Tensor &x,
     const at::Tensor &scale,
@@ -41,4 +44,40 @@ at::Tensor adaln_mindie_sd_impl_npu(
 
     EXEC_NPU_CMD<ADALAYER_NORM_OP_NAME>(x, scale, shift, weight, bias, epsilon, output);
     return output;
+}
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor> adaln_v2_mindie_sd_impl_npu(
+    const at::Tensor &x,
+    const at::Tensor &scale,
+    const at::Tensor &shift,
+    const c10::optional<at::Tensor> &weight_opt,
+    const c10::optional<at::Tensor> &bias_opt,
+    const c10::optional<double> &epsilon_opt)
+{
+    TORCH_CHECK(x.dim() == EXPECTED_TENSOR_DIMENSION, "x must be 3D tensor");
+
+    double epsilon = epsilon_opt.value_or(1e-5);
+    const at::Tensor& weight = c10::value_or_else(weight_opt, [] {return at::Tensor();});
+    const at::Tensor& bias = c10::value_or_else(bias_opt, [] {return at::Tensor();});
+
+    at::Tensor output = at_npu::native::empty_with_format(
+        x.sizes(),
+        x.options(),
+        at_npu::native::get_npu_format(x)
+    );
+    at::Tensor mean_out;
+    at::Tensor rstd_out;
+    at::SmallVector<int64_t, 8> mean_shape; // 维度最大支持到8
+
+    // shape and dtype of mean and variance depend on x dtype
+    for (size_t index = 0; index < x.dim() - 1; index++) {
+        mean_shape.emplace_back(x.size(index));
+    }
+    mean_shape.emplace_back(1);
+    mean_out =
+        at_npu::native::empty_with_format(mean_shape, x.options(), at_npu::native::get_npu_format(x));
+    rstd_out =
+        at_npu::native::empty_with_format(mean_shape, x.options(), at_npu::native::get_npu_format(x));
+    EXEC_NPU_CMD<ADALAYER_NORM_V2_OP_NAME>(x, scale, shift, weight, bias, epsilon, output, mean_out, rstd_out);
+    return std::tie(output, mean_out, rstd_out);
 }
--- csrc/plugin/register_ops.cpp
+++ csrc/plugin/register_ops.cpp
@@ -34,6 +34,9 @@ TORCH_LIBRARY(mindiesd, m)
     m.def("adaln(Tensor x, Tensor scale, Tensor shift, Tensor? weight=None, \
         Tensor? bias=None, float? epsilon=1e-5) \
         -> Tensor");
+    m.def("adaln_v2(Tensor x, Tensor scale, Tensor shift, Tensor? weight=None, \
+        Tensor? bias=None, float? epsilon=1e-5) \
+        -> (Tensor, Tensor, Tensor)");
     m.def("la_preprocess(Tensor query, Tensor key, Tensor value, int align_len=256) \
         -> (Tensor, Tensor, Tensor)");
     m.def("rainfusionattention(Tensor query, Tensor key, Tensor value, Tensor select_idx, \
@@ -65,6 +68,7 @@ TORCH_LIBRARY_IMPL(mindiesd, PrivateUse1, m)
     m.impl("rope", &rope_mindie_sd_impl_npu);
     m.impl("la", &la_mindie_sd_impl_npu);
     m.impl("adaln", &adaln_mindie_sd_impl_npu);
+    m.impl("adaln_v2", &adaln_v2_mindie_sd_impl_npu);
     m.impl("la_preprocess", &la_preprocess_mindie_sd_impl_npu);
     m.impl("rainfusionattention", &rainfusionattention_mindie_sd_impl_npu);
     m.impl("block_sparse_attention", &block_sparse_attention_impl_npu);
